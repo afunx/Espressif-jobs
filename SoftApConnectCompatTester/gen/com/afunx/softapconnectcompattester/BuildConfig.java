b93c90d20a5f382840e2962f0c1b4165e8adff00
afunx <afun1989@gmail.com>
2016-09-06 14:16:19 +0800

    SoftApConnectCompatTester: remove gen/ files

--- SoftApConnectCompatTester/gen/com/afunx/softapconnectcompattester/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.afunx.softapconnectcompattester;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
