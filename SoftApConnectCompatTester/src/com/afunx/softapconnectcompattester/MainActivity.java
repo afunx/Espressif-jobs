1d0b7a026eae97621350d0f338cd9ec123e24ec8
afunx <afun1989@gmail.com>
2016-09-06 15:22:42 +0800

    SoftApConnectCompatTester: remove xml parser test code in MainActivity

--- SoftApConnectCompatTester/src/com/afunx/softapconnectcompattester/MainActivity.java
+++ SoftApConnectCompatTester/src/com/afunx/softapconnectcompattester/MainActivity.java
@@ -1,16 +1,10 @@
 package com.afunx.softapconnectcompattester;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import com.afunx.xml.model.SoftApXmlModel;
-import com.afunx.xml.model.parser.DomSoftApXmlParser;
-import com.afunx.xml.model.parser.PullSoftApXmlParser;
-import com.afunx.xml.model.parser.SaxSoftApXmlParser;
-import com.afunx.xml.model.parser.SoftApXmlParser;
 import com.afunx.xml.model.persistence.SoftApPersistentor;
 
 import android.app.Activity;
@@ -111,71 +105,4 @@ public class MainActivity extends Activity {
 		}
 		
 	}
-
-	/**
-	 * just for test xml parser start
-	 */
-	private void testXmlParser() {
-		try {
-			XmlTester.test(getAssets().open("softaps.xml"));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static class XmlTester {
-		public static void test(InputStream is) {
-//			testSaxParser(is);
-//			testDomParser(is);
-//			testPullParser(is);
-		}
-
-		private static void testSaxParser(InputStream is) {
-			log.debug("testSaxParser()");
-			SoftApXmlParser parser = new SaxSoftApXmlParser();
-			try {
-				List<SoftApXmlModel> softaps = parser.parse(is);
-				for(SoftApXmlModel softap: softaps) {
-					log.info("softap:"+softap);
-				}
-				String softapsSerial = parser.serialize(softaps);
-				log.info(softapsSerial);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		private static void testDomParser(InputStream is) {
-			log.debug("testDomParser()");
-			SoftApXmlParser parser = new DomSoftApXmlParser();
-			try {
-				List<SoftApXmlModel> softaps = parser.parse(is);
-				for(SoftApXmlModel softap: softaps) {
-					log.info("softap:"+softap);
-				}
-				String softapsSerial = parser.serialize(softaps);
-				log.info(softapsSerial);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		
-		private static void testPullParser(InputStream is) {
-			log.debug("testPullParser()");
-			SoftApXmlParser parser = new PullSoftApXmlParser();
-			try {
-				List<SoftApXmlModel> softaps = parser.parse(is);
-				for(SoftApXmlModel softap: softaps) {
-					log.info("softap:"+softap);
-				}
-				String softapsSerial = parser.serialize(softaps);
-				log.info(softapsSerial);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
-	/**
-	 * just for test xml parser end
-	 */
 }
